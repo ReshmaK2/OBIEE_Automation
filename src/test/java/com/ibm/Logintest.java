a29494fee8fbaa0dfe736909f501323649980929
Reshma K <reshma.k2@ibm.com>
2024-05-20 15:32:20 +0530

    chrome driver change

--- src/test/java/com/ibm/Logintest.java
+++ src/test/java/com/ibm/Logintest.java
@@ -1,32 +1,35 @@
 package com.ibm;
+
 import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class Logintest {
-	
-	public static WebDriver driver;
-	
-	@Test
-    public static void OBIEE() throws InterruptedException, IOException {
- 
-    	String chromeDriverPath = System.getProperty("user.dir") + "/src/resources/chromedriver.exe";
-    	System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-        driver = new ChromeDriver();
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-        //EX portal
-        
-        driver.get("https://reports.ex.indianoil.in/analytics");
-        
-        Actions.reportsEX();
-        
-        //PX portal
-       
-        driver.get("https://reports.px.indianoil.in/analytics");
-        
-        Actions.reportsPX();
-        driver.quit();
+public class Logintest {
+
+	public static WebDriver driver;
+
+	@Test
+	public static void OBIEE() throws InterruptedException, IOException {
+
+		WebDriverManager.chromedriver().setup();
+
+		driver = new ChromeDriver();
+
+		// EX portal
+
+		driver.get("https://reports.ex.indianoil.in/analytics");
+
+		Actions.reportsEX();
+
+		// PX portal
+
+		driver.get("https://reports.px.indianoil.in/analytics");
+
+		Actions.reportsPX();
+		driver.quit();
 	}
 }
