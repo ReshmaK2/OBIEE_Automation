c3165370771ddabe45eaf16bc3c506a8244a193d
Reshma K <reshma.k2@ibm.com>
2024-05-20 15:37:31 +0530

    compile err

--- src/test/java/com/ibm/Logintest.java
+++ src/test/java/com/ibm/Logintest.java
@@ -6,18 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-<<<<<<< HEAD
 import io.github.bonigarcia.wdm.WebDriverManager;
-=======
-public class Logintest {
-	
-	public static WebDriver driver;
-	
-	@Test
-    public static void OBIEE() throws InterruptedException, IOException {
- 
-        driver = new ChromeDriver();
->>>>>>> 215b05f52ef9a4202eb6289b00596e8a2991e641
 
 public class Logintest {
 
